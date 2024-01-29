965cd6d1657f034a410325e1a27b20b476f69c80
Maximon0101 <maximon2006@gmail.com>
2024-01-30 00:30:06 +0300

    BankCard Class is ready!

--- app/src/main/java/pro/maximon/percentages/BankCard.kt
+++ app/src/main/java/pro/maximon/percentages/BankCard.kt
@@ -2,21 +2,42 @@ package pro.maximon.percentages
 
 import android.content.Context
 import android.view.View
+import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class BankCard {
     private val view : View;
     private val deposit_text_input : TextInputEditText;
     private val percentage_text_input : TextInputEditText;
+    private val deposit_layout : TextInputLayout;
+    private val percetage_layout : TextInputLayout;
 
     constructor(_view : View, _context : Context) {
         view = _view;
         deposit_text_input = view.findViewById(R.id.bank_card_deposit_text);
         percentage_text_input = view.findViewById(R.id.bank_card_percentage_text);
+        deposit_layout = view.findViewById(R.id.bank_card_deposit);
+        percetage_layout = view.findViewById(R.id.bank_card_percentage);
+
 
         deposit_text_input.addTextChangedListener {
+            val str : String = deposit_text_input.text.toString();
+            if (!isNumber(str)) {
+                deposit_layout.error = _context.getString(R.string.input_error)
+            } else {
+                deposit_layout.error = null;
+            }
+        }
 
+        percentage_text_input.addTextChangedListener {
+            val str : String = percentage_text_input.text.toString();
+            if (!isNumber(str)) {
+                percetage_layout.error = _context.getString(R.string.input_error)
+            } else {
+                percetage_layout.error = null;
+            }
         }
     }
 }
---
+++ app/src/main/java/pro/maximon/percentages/MathUtils.kt
@@ -0,0 +1,12 @@
+package pro.maximon.percentages
+
+fun isNumber(string: String) : Boolean {
+    var count : Int = 0;
+
+    for (char in string) {
+        if (char !in "0123456789.,") { return false };
+        if (char in ".,") { count++ }
+    }
+    if (count > 1) { return false };
+    return true;
+}
